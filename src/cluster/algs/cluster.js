e0b78e930ed9705c2065b8f06a45a2f258dcc16d
Stas <pfqxbr2208@gmail.com>
2024-03-24 14:04:15 +0700

    created drawPoint and clearCanvas js files, trying to draw a point with js

--- src/cluster/algs/cluster.js
+++ src/cluster/algs/cluster.js
@@ -1,3 +1,20 @@
+import {drawPointIntoCanvas} from "../../shared/ui/drawPointIntoCanvas.js";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const colorPicker = document.getElementById("colorPicker");
+const radius = 3.0;
+
+canvas.addEventListener("click", function(event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const color = colorPicker.value;
+    const params = { radius, color };
+    drawPointIntoCanvas(x, y, params, ctx);
+});
+
+
 function cluster () {
 
 }
---
+++ src/shared/ui/drawPointIntoCanvas.js
@@ -0,0 +1,17 @@
+const radius = 3.0;
+export function drawPointIntoCanvas(x,y,params, ctx){
+
+    if(params.radius !== undefined){
+        ctx.beginPath();
+        ctx.arc(x,y,params.radius,0,Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+
+    }else{
+        ctx.beginPath();
+        ctx.arc(x,y,radius,0,Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+}
